849bff21c3f9904c0929526526c979bd6a523749
Lykrast <aymericpm@gmail.com>
2018-10-02 09:38:20 +0200

    Added Harvester's Scythe by using CoFH code

--- src/main/java/lykrast/harvestersnight/HarvestersNight.java
+++ src/main/java/lykrast/harvestersnight/HarvestersNight.java
@@ -3,33 +3,61 @@ package lykrast.harvestersnight;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraft.init.Blocks;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = HarvestersNight.MODID, 
 	name = HarvestersNight.NAME, 
 	version = HarvestersNight.VERSION, 
 	acceptedMinecraftVersions = "[1.12, 1.13)")
+@Mod.EventBusSubscriber
 public class HarvestersNight {
     public static final String MODID = "harvestersnight";
     public static final String NAME = "Harvester's Night";
     public static final String VERSION = "@VERSION@";
 
 	public static Logger logger = LogManager.getLogger(MODID);
+	
+	//Shoving everything in this class since it's not gonna be a big mod
+	public static ToolMaterial harvesterMaterial;
+	public static Item harvesterScythe;
+
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event) {
+		//Only one item so inlining my usual helper functions
+		harvesterMaterial = EnumHelper.addToolMaterial("harvester", 
+				ToolMaterial.IRON.getHarvestLevel(), 
+				1323, 
+				ToolMaterial.IRON.getEfficiency(), 
+				ToolMaterial.IRON.getAttackDamage(), 
+				ToolMaterial.IRON.getEnchantability());
+		harvesterScythe = new ItemHarvesterScythe(harvesterMaterial)
+				.setRegistryName(new ResourceLocation(MODID, "harvester_scythe"))
+				.setUnlocalizedName(MODID + ".harvester_scythe")
+				.setCreativeTab(CreativeTabs.TOOLS);
+		event.getRegistry().register(harvesterScythe);
+	}
+	
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public static void registerModels(ModelRegistryEvent evt) {
+		ModelLoader.setCustomModelResourceLocation(harvesterScythe, 0, new ModelResourceLocation(harvesterScythe.getRegistryName(), "inventory"));
+	}
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-		
-    }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    public void init(FMLInitializationEvent event) {
     }
 }
---
+++ src/main/java/lykrast/harvestersnight/ItemHarvesterScythe.java
@@ -0,0 +1,136 @@
+package lykrast.harvestersnight;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraft.network.play.client.CPacketPlayerDigging;
+import net.minecraft.network.play.server.SPacketBlockChange;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeHooks;
+
+public class ItemHarvesterScythe extends ItemSword {
+
+	public ItemHarvesterScythe(ToolMaterial material) {
+		super(material);
+	}
+	
+    public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving) {
+        if (state.getBlockHardness(worldIn, pos) != 0) stack.damageItem(1, entityLiving);
+        return true;
+    }
+    
+    public boolean canHarvestBlock(IBlockState blockIn) {
+        Block block = blockIn.getBlock();
+        if (block == Blocks.WEB || block == Blocks.VINE || block == Blocks.LEAVES || block == Blocks.LEAVES2) return true;
+        Material material = blockIn.getMaterial();
+        return material == Material.LEAVES || material == Material.PLANTS || material == Material.VINE || material == Material.WEB;
+    }
+    
+    //Adapted from the CoFH Core sickles
+    //https://github.com/CoFH/CoFHCore/blob/master/src/main/java/cofh/core/item/tool/ItemSickleCore.java
+    @Override
+	public boolean onBlockStartBreak(ItemStack stack, BlockPos pos, EntityPlayer player) {
+		World world = player.world;
+		IBlockState state = world.getBlockState(pos);
+
+		if (!canHarvestBlock(state, stack)) {
+			if (!player.capabilities.isCreativeMode) {
+				stack.damageItem(1, player);
+			}
+			return false;
+		}
+		
+		int x = pos.getX();
+		int y = pos.getY();
+		int z = pos.getZ();
+
+		int used = 0;
+		world.playEvent(2001, pos, Block.getStateId(state));
+
+		//7x3x7
+		for (int i = x - 3; i <= x + 3; i++) {
+			for (int j = z - 3; j <= z + 3; j++) {
+				for (int k = y - 1; k <= y + 1; k++) {
+					if (harvestBlock(world, new BlockPos(i, k, j), player)) used++;
+				}
+			}
+		}
+		if (used > 0 && !player.capabilities.isCreativeMode) {
+			stack.damageItem(used, player);
+		}
+		return false;
+	}
+    
+    //Sickle logic uses it so copied it too, still from CoFH Core
+    //https://github.com/CoFH/CoFHCore/blob/master/src/main/java/cofh/core/item/tool/ItemToolCore.java
+    protected boolean harvestBlock(World world, BlockPos pos, EntityPlayer player) {
+
+		if (world.isAirBlock(pos)) {
+			return false;
+		}
+		EntityPlayerMP playerMP = null;
+		if (player instanceof EntityPlayerMP) {
+			playerMP = (EntityPlayerMP) player;
+		}
+		IBlockState state = world.getBlockState(pos);
+		Block block = state.getBlock();
+		// only effective materials
+		if (!canHarvestBlock(state, player.getHeldItemMainhand())) {
+			return false;
+		}
+		if (!ForgeHooks.canHarvestBlock(block, player, world, pos)) {
+			return false;
+		}
+		// send the blockbreak event
+		int xpToDrop = 0;
+		if (playerMP != null) {
+			xpToDrop = ForgeHooks.onBlockBreakEvent(world, playerMP.interactionManager.getGameType(), playerMP, pos);
+			if (xpToDrop == -1) {
+				return false;
+			}
+		}
+		// Creative Mode
+		//TODO: check why this is not working, might be an issue to report to cofh
+		if (player.capabilities.isCreativeMode) {
+			if (!world.isRemote) {
+				if (block.removedByPlayer(state, world, pos, player, false)) {
+					block.onBlockDestroyedByPlayer(world, pos, state);
+				}
+				// always send block update to client
+				playerMP.connection.sendPacket(new SPacketBlockChange(world, pos));
+			} else {
+				if (block.removedByPlayer(state, world, pos, player, false)) {
+					block.onBlockDestroyedByPlayer(world, pos, state);
+				}
+				Minecraft.getMinecraft().getConnection().sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.START_DESTROY_BLOCK, pos, Minecraft.getMinecraft().objectMouseOver.sideHit));
+			}
+		}
+		// Otherwise
+		if (!world.isRemote) {
+			if (block.removedByPlayer(state, world, pos, player, true)) {
+				block.onBlockDestroyedByPlayer(world, pos, state);
+				block.harvestBlock(world, player, pos, state, world.getTileEntity(pos), player.getHeldItemMainhand());
+				if (xpToDrop > 0) {
+					block.dropXpOnBlockBreak(world, pos, xpToDrop);
+				}
+			}
+			// always send block update to client
+			playerMP.connection.sendPacket(new SPacketBlockChange(world, pos));
+		} else {
+			if (block.removedByPlayer(state, world, pos, player, true)) {
+				block.onBlockDestroyedByPlayer(world, pos, state);
+			}
+			Minecraft.getMinecraft().getConnection().sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.START_DESTROY_BLOCK, pos, Minecraft.getMinecraft().objectMouseOver.sideHit));
+		}
+		return true;
+	}
+
+}
